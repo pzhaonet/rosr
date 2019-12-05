b6bbbf02fadacc600b51f464da37a576ebef1315
Peng Zhao <pzhao@pzhao.net>
2019-12-05 11:59:39 +0100

    support Chinese

--- R/rmd.R
+++ R/rmd.R
@@ -13,13 +13,12 @@ rmd_template <- function(...,
                          template_name,
                          keep_tex = TRUE,
                          citation_package = "natbib",
-                         latex_engine = "xelatex",
                          md_extensions = c(
                            "-autolink_bare_uris", # disables automatic links, needed for plain email in \correspondence
                            "-auto_identifiers"    # disables \hypertarget commands
                          )) {
   template <- system.file("rmarkdown", "templates", template_name, "resources", "template.tex", package = "rosr")
-  fmt <- rmarkdown::pdf_document(..., template = template, keep_tex = keep_tex, latex_engine = latex_engine)
+  fmt <- rmarkdown::pdf_document(..., template = template, keep_tex = keep_tex)
   fmt$inherits <- "pdf_document"
   fmt
 }
@@ -27,41 +26,41 @@ rmd_template <- function(...,
 #' @rdname rmd_template
 #' @export
 statement_svm <- function(...){
-  rmd_template(template_name = "statement_svm")
+  rmd_template(template_name = "statement_svm", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 article_svm <- function(...){
-  rmd_template(template_name = "article_svm")
+  rmd_template(template_name = "article_svm", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 cv_svm <- function(...){
-  rmd_template(template_name = "cv_svm")
+  rmd_template(template_name = "cv_svm", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 syllabus_svm <- function(...){
-  rmd_template(template_name = "syllabus_svm")
+  rmd_template(template_name = "syllabus_svm", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 manuscript_svm <- function(...){
-  rmd_template(template_name = "manuscript_svm")
+  rmd_template(template_name = "manuscript_svm", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 letter_moderncv <- function(...){
-  rmd_template(template_name = "letter_moderncv")
+  rmd_template(template_name = "letter_moderncv", ...)
 }
 
 #' @rdname rmd_template
 #' @export
 beamer_zh <- function(...){
-  rmd_template(template_name = "beamer_zh")
+  rmd_template(template_name = "beamer_zh", ...)
 }
